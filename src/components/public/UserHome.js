9b1f75248037b0e615cdaacb57d4b321e072d624
elena.zingalemagro <elena.zingalemagro@gmail.com>
2021-05-26 16:25:40 +0200

    modifiche css userhome

--- src/components/public/UserHome.js
+++ src/components/public/UserHome.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import "../../styles/LoginStyle.css";
 
 const UserHome = () => {
   return (
     <div>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
+      <Card className="imgwinery">
+        <Card.Img
+          variant="top"
+          src="https://i0.wp.com/zeroaxis-studios.com/wp-content/uploads/2020/11/ubaldo-bitumi-SecR2ak__yU-unsplash-scaled-e1606060082667.jpg?fit=1280%2C746&ssl=1%27"
+        />
       </Card>
-      <br />
-      <Card>
-        <Card.Body>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-        <Card.Img variant="bottom" src="holder.js/100px180" />
+
+      <Card className="container">
+        <Card.Text className="null">Scopri la nostra storia</Card.Text>
+        <Card.Text className="nuff">
+          Tutto ciò che ci rende unici tra vigneti, vini e cantine
+        </Card.Text>
       </Card>
     </div>
   );
